58eb6fca206cf2954b59731e82f182d79de069e0
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-03 07:39:17 -0600

    added reactionCount virtual to thoughts model

--- models/Thought.js
+++ models/Thought.js
@@ -25,6 +25,12 @@ const thoughtSchema = new Schema(
     }
 );
 
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    })
+
 const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
